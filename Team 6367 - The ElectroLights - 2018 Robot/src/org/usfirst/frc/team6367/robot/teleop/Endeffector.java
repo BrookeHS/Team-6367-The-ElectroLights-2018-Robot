f7eaa2e0d6d3c5ef664cf071628359dff2a49053
estudent <estudent@ebrooke.org>
2018-02-10 15:50:51 -0500

    EndEffector
    We made code that includes basic endEffector functions.

--- Team 6367 - The ElectroLights - 2018 Robot/src/org/usfirst/frc/team6367/robot/teleop/Endeffector.java
+++ Team 6367 - The ElectroLights - 2018 Robot/src/org/usfirst/frc/team6367/robot/teleop/Endeffector.java
@@ -1,14 +1,19 @@
 package org.usfirst.frc.team6367.robot.teleop;
 
+import org.usfirst.frc.team6367.robot.io.RobotOutput;
+
 public class Endeffector {
-	
+	RobotOutput RobotOut;
+	public Endeffector() {
+		RobotOut = RobotOutput.getInstance();
+	}
 	public void  deployBox() {
-		
+		RobotOut.setEndEffector(.5);
 	}
-	public void Intake() {
-		
+	public void intake() {
+		RobotOut.setEndEffector(-.5);
 	}
-	public void Stop() {
-		
+	public void stop() {
+		RobotOut.setEndEffector(0);
 	}
 }
