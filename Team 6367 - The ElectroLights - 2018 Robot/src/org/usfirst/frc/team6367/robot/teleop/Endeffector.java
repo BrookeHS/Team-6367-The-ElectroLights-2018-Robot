d380af164082ab2470211e9a2f534c5bb6d18b28
janeyawash <janeya.washington@ebrooke.org>
2018-02-10 15:37:25 -0500

    Create methods for endeffectors

---
+++ Team 6367 - The ElectroLights - 2018 Robot/src/org/usfirst/frc/team6367/robot/teleop/Endeffector.java
@@ -0,0 +1,14 @@
+package org.usfirst.frc.team6367.robot.teleop;
+
+public class Endeffector {
+	
+	public void  deployBox() {
+		
+	}
+	public void Intake() {
+		
+	}
+	public void Stop() {
+		
+	}
+}
