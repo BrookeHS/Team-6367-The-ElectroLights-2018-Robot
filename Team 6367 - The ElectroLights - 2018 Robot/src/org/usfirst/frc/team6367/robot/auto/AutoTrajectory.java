481513a11a555dfb2c6009e021720b04832ceec7
JonnyG511 <35736713+JonnyG511@users.noreply.github.com>
2018-02-18 15:11:38 -0500

    auto trajectory class
    we put code for the path finder.

--- Team 6367 - The ElectroLights - 2018 Robot/src/org/usfirst/frc/team6367/robot/auto/AutoTrajectory.java
+++ Team 6367 - The ElectroLights - 2018 Robot/src/org/usfirst/frc/team6367/robot/auto/AutoTrajectory.java
@@ -11,110 +11,102 @@ import jaci.pathfinder.followers.EncoderFollower;
 import jaci.pathfinder.modifiers.TankModifier;
 
 public class AutoTrajectory {
-	 EncoderFollower left;
-	  EncoderFollower right;
-	  @MagicInject
-		RobotOutput robotOut;
-	  @MagicInject
-	  SensorInput sensor;
-	  
-	  public void calculateTrajectory( int startingLocation, boolean scaleSide ){
-	    Trajectory[] toReturn = new Trajectory[2];
-			Trajectory.Config config = new Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC, Trajectory.Config.SAMPLES_HIGH, 0.02, 1.7, 2.0, 60.0);
-	    
-	    /* Determine waypoints based on robot starting location and the side with
-	     * the scale.
-	     * Location -> left and Scale -> left.
-	     *   Move forward and drop cube.
-	     * Location -> left and Scale -> right.
-	     *   Move forward, turn right, move forward, and then drop cube.
-	     * Location -> middle and Scale -> left.
-	     *   Turn left, move forward, turn right, move forward, drop cube.
-	     * Location -> middle and Scale -> right.
-	     *   Turn right, move forward, turn left, move forward, drop cube.
-	     * Location -> right and Scale -> left.
-	     *   Move forward and drop cube.
-	     * Location -> right and Scale -> right.
-	     *   Move forward, turn left, move forward, and then drop cube.
-	     */
-	    Waypoint[] points;
-	    if( startingLocation==0&&scaleSide==true ){
-	      points = new Waypoint[] {
-	        new Waypoint(-4, -1, Pathfinder.d2r(-45)),
-	        new Waypoint(-2, -2, 0),
-	        new Waypoint(0, 0, 0)
-	      };
-	    }
-	    else if(startingLocation==0&&scaleSide==false){
-	      points = new Waypoint[] {
-	        new Waypoint(-4, -1, Pathfinder.d2r(-45)),
-	        new Waypoint(-2, -2, 0),
-	        new Waypoint(0, 0, 0)
-	      };
-	    }
-	    else if(startingLocation==1&&scaleSide==true){
-	      points = new Waypoint[] {
-	        new Waypoint(-4, -1, Pathfinder.d2r(-45)),
-	        new Waypoint(-2, -2, 0),
-	        new Waypoint(0, 0, 0)
-	      };
-	    }
-	    else if(startingLocation==1&&scaleSide==false){
-	      points = new Waypoint[] {
-	        new Waypoint(-4, -1, Pathfinder.d2r(-45)),
-	        new Waypoint(-2, -2, 0),
-	        new Waypoint(0, 0, 0)
-	      };
-	    }
-	    else if(startingLocation==2&&scaleSide==true ){
-	      points = new Waypoint[] {
-	        new Waypoint(-4, -1, Pathfinder.d2r(-45)),
-	        new Waypoint(-2, -2, 0),
-	        new Waypoint(0, 0, 0)
-	      };
-	    }
-	    else if(startingLocation==2&&scaleSide==false ){
-	      points = new Waypoint[] {
-	        new Waypoint(-4, -1, Pathfinder.d2r(-45)),
-	        new Waypoint(-2, -2, 0),
-	        new Waypoint(0, 0, 0)
-	      };
-	    }
-	    else{
-	        points = new Waypoint[] {
-	        new Waypoint(-4, -1, Pathfinder.d2r(-45)),
-	        new Waypoint(-2, -2, 0),
-	        new Waypoint(0, 0, 0)
-	      };
-	    }
 
-	    Trajectory trajectory = Pathfinder.generate(points, config);
+	public static final int kENCODERTICKSPERREV = 1024;
+	public static final double kWHEELDIAMETER = 0.1524;
+	public static final double kP_ENCODER = 0.8;
+	public static final double kI_ENCODER = 0.0;
+	public static final double kD_ENCODER = 0.0;
+	public static final double kMAXVELOCITY = 1.7;
 
-	    // Wheelbase Width = 0.5969m
-	    TankModifier modifier = new TankModifier(trajectory).modify(0.5969);
+	EncoderFollower left;
+	EncoderFollower right;
 
-	    // Visit : https://github.com/JacisNonsense/Pathfinder/wiki/Pathfinder-for-FRC---Java
-	    // Figure out to setup encoders.
-	   	EncoderFollower left = new EncoderFollower(modifier.getLeftTrajectory());
-			EncoderFollower right = new EncoderFollower(modifier.getRightTrajectory());
-		  left.configureEncoder(robotOut.getEncoderLeftSide(), 1000, .1524);
-	    right.configureEncoder(robotOut.getEncoderRightSide(), 1000, .1524);
-			left.configurePIDVA(1.0, 0.0, 0.0, 1 / 1.7, 0);
-	  	right.configurePIDVA(1.0, 0.0, 0.0, 1 / 1.7, 0);
-	  	double outputLeftEncoder = left.calculate(robotOut.getEncoderLeftSide());
-	  	double outputRightEncoder = right.calculate(robotOut.getEncoderRightSide());
-	  	double l = left.calculate(robotOut.getEncoderLeftSide());
-		double r = right.calculate(robotOut.getEncoderRightSide());
+	@MagicInject
+	SensorInput sensors;
 
-		double ang = sensor.ahrs.getAngle(); // Assuming the gyro is giving a value in degrees
-		double desired_heading = Pathfinder.r2d(left.getHeading());  // Should also be in degrees
+	@MagicInject
+	RobotOutput robotOut;
 
-		double angleDifference = Pathfinder.boundHalfDegrees(desired_heading - ang);
-	  double turn = 0.8 * (-1.0/80.0) * angleDifference;
+	public void calculateTrajectory(int startingLocation, boolean scaleSide) {
 
-	robotOut.setDriveLeft(l + turn);
-	robotOut.setDriveRight(r - turn);
-	  }
-	  
-	  
+		Trajectory.Config config = new Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC,
+				Trajectory.Config.SAMPLES_HIGH, 0.02, AutoTrajectory.kMAXVELOCITY, 2.0, 60.0);
+
+		/*
+		 * Determine waypoints based on robot starting location and the side with the
+		 * scale. Location -> left and Scale -> left. Move forward and drop cube.
+		 * Location -> left and Scale -> right. Move forward, turn right, move forward,
+		 * and then drop cube. Location -> middle and Scale -> left. Turn left, move
+		 * forward, turn right, move forward, drop cube. Location -> middle and Scale ->
+		 * right. Turn right, move forward, turn left, move forward, drop cube. Location
+		 * -> right and Scale -> left. Move forward and drop cube. Location -> right and
+		 * Scale -> right. Move forward, turn left, move forward, and then drop cube.
+		 */
+		Waypoint[] points;
+		if (startingLocation == 0 && scaleSide == true) {
+			points = new Waypoint[] { new Waypoint(-4, -1, Pathfinder.d2r(-45)), new Waypoint(-2, -2, 0),
+					new Waypoint(0, 0, 0) };
+		} else if (startingLocation == 0 && scaleSide == false) {
+			points = new Waypoint[] { new Waypoint(-4, -1, Pathfinder.d2r(-45)), new Waypoint(-2, -2, 0),
+					new Waypoint(0, 0, 0) };
+		} else if (startingLocation == 1 && scaleSide == true) {
+			points = new Waypoint[] { new Waypoint(-4, -1, Pathfinder.d2r(-45)), new Waypoint(-2, -2, 0),
+					new Waypoint(0, 0, 0) };
+		} else if (startingLocation == 1 && scaleSide == false) {
+			points = new Waypoint[] { new Waypoint(-4, -1, Pathfinder.d2r(-45)), new Waypoint(-2, -2, 0),
+					new Waypoint(0, 0, 0) };
+		} else if (startingLocation == 2 && scaleSide == true) {
+			points = new Waypoint[] { new Waypoint(-4, -1, Pathfinder.d2r(-45)), new Waypoint(-2, -2, 0),
+					new Waypoint(0, 0, 0) };
+		} else if (startingLocation == 2 && scaleSide == false) {
+			points = new Waypoint[] { new Waypoint(-4, -1, Pathfinder.d2r(-45)), new Waypoint(-2, -2, 0),
+					new Waypoint(0, 0, 0) };
+		} else {
+			points = new Waypoint[] { new Waypoint(-4, -1, Pathfinder.d2r(-45)), new Waypoint(-2, -2, 0),
+					new Waypoint(0, 0, 0) };
+		}
+
+		// After you generate the waypoints, define the trajectory.
+		Trajectory trajectory = Pathfinder.generate(points, config);
+
+		// Modifies trajectory based on how wide the wheels are.
+		// Wheelbase Width = 0.5969
+		TankModifier modifier = new TankModifier(trajectory).modify(0.5969);
+
+		// Creates two EncoderFollower objects for the left and the right side of the
+		// robot.
+		left = new EncoderFollower(modifier.getLeftTrajectory());
+		left.configureEncoder(robotOut.getEncoderLeftSide(), AutoTrajectory.kENCODERTICKSPERREV,
+				AutoTrajectory.kWHEELDIAMETER);
+		left.configurePIDVA(AutoTrajectory.kP_ENCODER, AutoTrajectory.kI_ENCODER, AutoTrajectory.kD_ENCODER,
+				1 / AutoTrajectory.kMAXVELOCITY, 0);
+
+		right = new EncoderFollower(modifier.getRightTrajectory());
+		right.configureEncoder(robotOut.getEncoderRightSide(), AutoTrajectory.kENCODERTICKSPERREV,
+				AutoTrajectory.kWHEELDIAMETER);
+		right.configurePIDVA(AutoTrajectory.kP_ENCODER, AutoTrajectory.kI_ENCODER, AutoTrajectory.kD_ENCODER,
+				1 / AutoTrajectory.kMAXVELOCITY, 0);
 	}
+
+	public void move() {
+		// Figure out how fast the left side moves and figure out how fast the right
+		// side moves.
+		double outputLeftEncoder = left.calculate(robotOut.getEncoderLeftSide()); // incorrect
+		double outputRightEncoder = right.calculate(robotOut.getEncoderRightSide()); // incorrect
+
+		// Calculates the difference between our current angle and the desired angle.
+		double ang = sensors.ahrs.getAngle(); // Assuming the gyro is giving a value in degrees
+		double desired_heading = Pathfinder.r2d(left.getHeading()); // Should also be in degrees
+		double angleDifference = Pathfinder.boundHalfDegrees(desired_heading - ang);
+
+		// Interpolate the angleDifference (-180 to 180) to a range between (-1 and 1)
+		// Multiply that again by a factor of 0.8 to slow it down a bit.
+		double turn = 0.8 * (-1.0 / 180.0) * angleDifference;
+
+		//
+		robotOut.setDriveLeft(outputLeftEncoder + turn);
+		robotOut.setDriveRight(outputRightEncoder - turn);
+	}
+
+}
