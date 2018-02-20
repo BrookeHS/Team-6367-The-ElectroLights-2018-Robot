5bdf7d08c1018d2756320d09a3c60ca2e74c60d8
JonnyG511 <35736713+JonnyG511@users.noreply.github.com>
2018-02-20 11:54:53 -0500

    Added waypoints
    We had added the coordinates for the waypoints in meters.

--- PathGenerator/src/pg/PathGenerator.java
+++ PathGenerator/src/pg/PathGenerator.java
@@ -14,29 +14,57 @@ public class PathGenerator {
 		PathfinderLoader.load();
 		
 		generateAndWrite("robotLScaleL",
-			new Waypoint(0,0,0)
+		new Waypoint(0,7.31519976591361,0),
+     	new Waypoint(7.01039977566721,7.31519976591361,0),
+     	new Waypoint(8.22959973665281,6.40079979517441,-1.570797)
 		);
 
 		generateAndWrite("robotLScaleR",
-				new Waypoint(0,0,0)
+		new Waypoint(0,7.31519976591361,0),
+		new Waypoint(4.571999853696,7.61999975616001,0),
+      	new Waypoint(6.40079979517441,5.18159983418881,-1.570797),
+      	new Waypoint(6.40079979517441,1.8287999414784,-1.570797),
+      	new Waypoint(7.01039977566721,0.609599980492801,0),
+      	new Waypoint(8.22959973665281,1.523999951232,1.570797)
+      
 		);
 		
 		generateAndWrite("robotMScaleL",
-				new Waypoint(0,0,0)
+		new Waypoint(0,3.962399873,0),
+		new Waypoint(0.3047999902,3.962399873,0),
+		new Waypoint(4.267199863,7.315199766,0),
+		new Waypoint(7.315199766,7.315199766,0),
+		new Waypoint(8.229599737,6.400799795,-1.570797)
 		);
 		
 		generateAndWrite("robotMScaleR",
-				new Waypoint(0,0,0)
+		new Waypoint(0,3.9623998732032,0),
+		new Waypoint(2.7431999122176,0.914399970739201,0),
+		new Waypoint(7.01039977566721,0.609599980492801,0),
+		new Waypoint(8.22959973665281,1.8287999414784,1.570797)
 		);
 
 		generateAndWrite("robotRScaleL",
-				new Waypoint(0,0,0)
+        new Waypoint(0,0.914399970739201,0),
+        new Waypoint(4.571999853696,0.609599980492801,0),
+        new Waypoint(6.40079979517441,3.047999902464,-1.570797),
+        new Waypoint(6.40079979517441,6.40079979517441,-1.570797),
+        new Waypoint(7.01039977566721,7.61999975616001,0),
+        new Waypoint(8.22959973665281,6.40079979517441,1.570797)
+                
 		);
 
 		generateAndWrite("robotRScaleR",
-				new Waypoint(0,0,0)
+        new Waypoint(5.18159983418881,0.609599980492801,0),		
+        new Waypoint(7.31519976591361,0.609599980492801,0),		
+        new Waypoint(8.22959973665281,1.8287999414784,-1.570797)			
 		);	
-		
+
+  	generateAndWrite("default",
+        new Waypoint(0,0,0),		
+        new Waypoint(1,0,0)		
+		);	
+
 		
 		// TODO: generate more of these things
 	}
