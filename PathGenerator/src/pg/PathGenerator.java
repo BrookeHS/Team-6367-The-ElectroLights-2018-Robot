410004b6885e75dcf163810013dc71254f78617a
JonnyG511 <35736713+JonnyG511@users.noreply.github.com>
2018-03-10 13:45:49 -0500

    We changed the waypoints
    all of the trajectory waypoints were wrong and we fixed them.

--- PathGenerator/src/pg/PathGenerator.java
+++ PathGenerator/src/pg/PathGenerator.java
@@ -14,56 +14,51 @@ public class PathGenerator {
 		PathfinderLoader.load();
 		
 		generateAndWrite("robotLScaleL",
-		new Waypoint(0,7.31519976591361,0),
-     	new Waypoint(7.01039977566721,7.31519976591361,0),
-     	new Waypoint(8.22959973665281,6.40079979517441,-1.570797)
+			new Waypoint(0.487679984394241,6.55319979029761,0),
+			new Waypoint(5.33399982931201,6.85799978054401,0),
+			new Waypoint(7.13231977176577,5.94359980980481,0)
 		);
 
 		generateAndWrite("robotLScaleR",
-		new Waypoint(0,7.31519976591361,0),
-		new Waypoint(4.571999853696,7.61999975616001,0),
-      	new Waypoint(6.40079979517441,5.18159983418881,-1.570797),
-      	new Waypoint(6.40079979517441,1.8287999414784,-1.570797),
-      	new Waypoint(7.01039977566721,0.609599980492801,0),
-      	new Waypoint(8.22959973665281,1.523999951232,1.570797)
+			new Waypoint(0.487679984394241,6.70559978542081,0),
+			new Waypoint(4.571999853696,7.01039977566721,0),
+			new Waypoint(7.13231977176577,2.285999926848,0)
       
 		);
 		
 		generateAndWrite("robotMScaleL",
-		new Waypoint(0,3.962399873,0),
-		new Waypoint(0.3047999902,3.962399873,0),
-		new Waypoint(4.267199863,7.315199766,0),
-		new Waypoint(7.315199766,7.315199766,0),
-		new Waypoint(8.229599737,6.400799795,-1.570797)
+			new Waypoint(0.487679984394241,3.6575998829568,0),
+			new Waypoint(3.3527998927104,7.01039977566721,0),
+			new Waypoint(7.13231977176577,5.94359980980481,0)
 		);
 		
 		generateAndWrite("robotMScaleR",
-		new Waypoint(0,3.9623998732032,0),
-		new Waypoint(2.7431999122176,0.914399970739201,0),
-		new Waypoint(7.01039977566721,0.609599980492801,0),
-		new Waypoint(8.22959973665281,1.8287999414784,1.570797)
+			new Waypoint(0.487679984394241,3.6575998829568,0),
+			new Waypoint(4.2671998634496,1.523999951232,0),
+			new Waypoint(7.13231977176577,2.285999926848,0)
 		);
 
 		generateAndWrite("robotRScaleL",
-        new Waypoint(0,0.914399970739201,0),
-        new Waypoint(4.571999853696,0.609599980492801,0),
-        new Waypoint(6.40079979517441,3.047999902464,-1.570797),
-        new Waypoint(6.40079979517441,6.40079979517441,-1.570797),
-        new Waypoint(7.01039977566721,7.61999975616001,0),
-        new Waypoint(8.22959973665281,6.40079979517441,1.570797)
+			new Waypoint(0,0.914399970739201,0),
+			new Waypoint(4.571999853696,0.609599980492801,0),
+			new Waypoint(6.40079979517441,3.047999902464,-1.570797),
+			new Waypoint(6.40079979517441,6.40079979517441,-1.570797),
+			new Waypoint(7.01039977566721,7.61999975616001,0),
+			new Waypoint(8.22959973665281,1.523999951232,1.570797)
                 
 		);
 
 		generateAndWrite("robotRScaleR",
-        new Waypoint(5.18159983418881,0.609599980492801,0),		
-        new Waypoint(7.31519976591361,0.609599980492801,0),		
-        new Waypoint(8.22959973665281,1.8287999414784,-1.570797)			
+			new Waypoint(0.487679984394241,1.523999951232,0),
+			new Waypoint(3.9623998732032,1.523999951232,0),
+			new Waypoint(6.09599980492801,3.047999902464,-1.570797),
+			new Waypoint(7.19327976981505,5.79119981468161,0)			
 		);	
 
   	generateAndWrite("default",
-        new Waypoint(0,0,0),		
-        new Waypoint(1,0,0)		
-		);	
+	        new Waypoint(0,0,0),		
+	        new Waypoint(1,0,0)		
+	);	
 
 		
 		// TODO: generate more of these things
